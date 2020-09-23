775bed348dfcd4a67904ab79aa476000a6862707
JakubMarkiewicz <markiewicz.jakub2@gmail.com>
2020-09-24 00:05:12 +0200

    fix postinstall existing _export dir

--- .github/workflows/release-postinstall.js
+++ .github/workflows/release-postinstall.js
@@ -9,19 +9,19 @@
  * bits in the right place.
  */
 
-var path = require('path');
-var cp = require('child_process');
-var fs = require('fs');
-var os = require('os');
+var path = require("path");
+var cp = require("child_process");
+var fs = require("fs");
+var os = require("os");
 var platform = process.platform;
 
-var packageJson = require('./package.json');
+var packageJson = require("./package.json");
 var binariesToCopy = Object.keys(packageJson.bin)
   .map(function (name) {
     return packageJson.bin[name];
   })
-  .concat(['esyInstallRelease.js']);
-var foldersToCopy = ['bin', '_export'];
+  .concat(["esyInstallRelease.js"]);
+var foldersToCopy = ["bin", "_export"];
 
 function copyRecursive(srcDir, dstDir) {
   var results = [];
@@ -36,7 +36,7 @@ function copyRecursive(srcDir, dstDir) {
       try {
         fs.mkdirSync(dst);
       } catch (e) {
-        console.log('directory already exists: ' + dst);
+        console.log("directory already exists: " + dst);
         console.error(e);
       }
       results = results.concat(copyRecursive(src, dst));
@@ -63,15 +63,15 @@ function arch() {
   /**
    * The running binary is 64-bit, so the OS is clearly 64-bit.
    */
-  if (process.arch === 'x64') {
-    return 'x64';
+  if (process.arch === "x64") {
+    return "x64";
   }
 
   /**
    * All recent versions of Mac OS are 64-bit.
    */
-  if (process.platform === 'darwin') {
-    return 'x64';
+  if (process.platform === "darwin") {
+    return "x64";
   }
 
   /**
@@ -79,7 +79,7 @@ function arch() {
    * app is based on the presence of a WOW64 file: %SystemRoot%\SysNative.
    * See: https://twitter.com/feross/status/776949077208510464
    */
-  if (process.platform === 'win32') {
+  if (process.platform === "win32") {
     var useEnv = false;
     try {
       useEnv = !!(
@@ -87,29 +87,29 @@ function arch() {
       );
     } catch (err) {}
 
-    var sysRoot = useEnv ? process.env.SYSTEMROOT : 'C:\\Windows';
+    var sysRoot = useEnv ? process.env.SYSTEMROOT : "C:\\Windows";
 
     // If %SystemRoot%\SysNative exists, we are in a WOW64 FS Redirected application.
     var isWOW64 = false;
     try {
-      isWOW64 = !!fs.statSync(path.join(sysRoot, 'sysnative'));
+      isWOW64 = !!fs.statSync(path.join(sysRoot, "sysnative"));
     } catch (err) {}
 
-    return isWOW64 ? 'x64' : 'x86';
+    return isWOW64 ? "x64" : "x86";
   }
 
   /**
    * On Linux, use the `getconf` command to get the architecture.
    */
-  if (process.platform === 'linux') {
-    var output = cp.execSync('getconf LONG_BIT', { encoding: 'utf8' });
-    return output === '64\n' ? 'x64' : 'x86';
+  if (process.platform === "linux") {
+    var output = cp.execSync("getconf LONG_BIT", { encoding: "utf8" });
+    return output === "64\n" ? "x64" : "x86";
   }
 
   /**
    * If none of the above, assume the architecture is 32-bit.
    */
-  return 'x86';
+  return "x86";
 }
 
 // implementing it b/c we don't want to depend on fs.copyFileSync which appears
@@ -119,9 +119,9 @@ function copyFileSync(sourcePath, destPath) {
   try {
     data = fs.readFileSync(sourcePath);
   } catch (e) {
-    console.log("Couldn't find " + sourcePath + ' trying with .exe');
-    data = fs.readFileSync(sourcePath + '.exe');
-    sourcePath = sourcePath + '.exe';
+    console.log("Couldn't find " + sourcePath + " trying with .exe");
+    data = fs.readFileSync(sourcePath + ".exe");
+    sourcePath = sourcePath + ".exe";
   }
   var stat = fs.statSync(sourcePath);
   fs.writeFileSync(destPath, data);
@@ -129,7 +129,7 @@ function copyFileSync(sourcePath, destPath) {
 }
 
 var copyPlatformBinaries = (platformPath) => {
-  var platformBuildPath = path.join(__dirname, 'platform-' + platformPath);
+  var platformBuildPath = path.join(__dirname, "platform-" + platformPath);
 
   foldersToCopy.forEach((folderPath) => {
     var sourcePath = path.join(platformBuildPath, folderPath);
@@ -149,28 +149,35 @@ var copyPlatformBinaries = (platformPath) => {
   });
 };
 
-try {
-  fs.mkdirSync('_export');
-} catch (e) {
-  console.log('Could not create _export folder');
+if (fs.existsSync("_export")) {
+  // remove previous binary
+  fs.readdirSync("_export").forEach((file) => {
+    fs.unlinkSync(`_export/${file}`);
+  });
+} else {
+  try {
+    fs.mkdirSync("_export");
+  } catch (e) {
+    console.log("Could not create _export folder");
+  }
 }
 
 switch (platform) {
-  case 'win32':
-    if (arch() !== 'x64') {
-      console.warn('error: x86 is currently not supported on Windows');
+  case "win32":
+    if (arch() !== "x64") {
+      console.warn("error: x86 is currently not supported on Windows");
       process.exit(1);
     }
 
-    copyPlatformBinaries('windows-x64');
+    copyPlatformBinaries("windows-x64");
     break;
-  case 'linux':
-  case 'darwin':
+  case "linux":
+  case "darwin":
     copyPlatformBinaries(platform);
     break;
   default:
-    console.warn('error: no release built for the ' + platform + ' platform');
+    console.warn("error: no release built for the " + platform + " platform");
     process.exit(1);
 }
 
-require('./esyInstallRelease');
+require("./esyInstallRelease");
